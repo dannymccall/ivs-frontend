1d99fe1d9e1f05d70647184d9a738d42dd1581a3
Coding Expert <dannymccall419@gmail.com>
2024-08-16 15:13:43 +0000

    assignment view table restyled

--- src/Components/WidgetComponents/AassignmentsTableView.jsx
+++ src/Components/WidgetComponents/AassignmentsTableView.jsx
@@ -11,34 +11,35 @@ export default function AssignmentsTabaleView({
   return (
     <>
       <div className="table-container">
-        <table>
-          <thead>
-            <tr>
-              <th>Asset Name</th>
-              <th>Asset Model</th>
-              <th>Location Block</th>
-              <th>Location Room</th>
-              <th>Quantity</th>
-            </tr>
-          </thead>
-          <tbody>
-            {assignments.map((assignment) => {
-              return (
-                <tr key={assignment.id}>
-                  <td>
-                    {assignment.assetName.charAt(0).toUpperCase() +
-                      assignment.assetName.slice(1).toLowerCase()}
-                      
-                  </td>
-                  <td>{assignment.assetModel.toUpperCase()}</td>
-                  <td>{assignment.locationblock}</td>
-                  <td>{assignment.locationroom}</td>
-                  <td>{assignment.qty}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <div className="table-sub-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Asset Name</th>
+                <th>Asset Model</th>
+                <th>Location Block</th>
+                <th>Location Room</th>
+                <th>Quantity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {assignments.map((assignment) => {
+                return (
+                  <tr key={assignment.id} style={{ height: "40px" }}>
+                    <td>
+                      {assignment.assetName.charAt(0).toUpperCase() +
+                        assignment.assetName.slice(1).toLowerCase()}
+                    </td>
+                    <td>{assignment.assetModel.toUpperCase()}</td>
+                    <td>{assignment.locationblock}</td>
+                    <td>{assignment.locationroom}</td>
+                    <td>{assignment.qty}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
